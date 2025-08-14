0b4d7042147c08e442c27e9bb021b8547050907b
ayushchand18 <162828279+ayushchand18@users.noreply.github.com>
2025-08-14 14:11:08 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -23,7 +23,7 @@ const placeOrder = async (req, res) => {
         product_data: {
           name: item.name,
         },
-        unit_amount: item.price,
+        unit_amount: item.price*100,
       },
       quantity: item.quantity,
     }));
@@ -34,7 +34,7 @@ const placeOrder = async (req, res) => {
         product_data: {
           name: "Delivery Charges",
         },
-        unit_amount: 30,
+        unit_amount: 30*100,
       },
       quantity: 1,
     });
