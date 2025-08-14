04212f524362843d7073f6f2c79da342f07c2df5
ayushchand18 <162828279+ayushchand18@users.noreply.github.com>
2025-08-14 21:22:02 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -31,13 +31,13 @@ const Footer = () => {
           <h2>Get in touch</h2>
           <ul>
             <li>+92-308-4900522</li>
-            <li>contact@tomato.com</li>
+            <li>contact@mealio.com</li>
           </ul>
         </div>
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2024 @ Tomato.com - All Right Reserved.
+        Copyright 2024 @ Mealio.com - All Right Reserved.
       </p>
     </div>
   );
